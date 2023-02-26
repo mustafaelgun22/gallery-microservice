6e25cdcc64d2b181325289c3ae2d6b97001b8496
mustafa <elgunmustafa2@gmail.com>
2023-02-26 18:16:27 +0300

    unused import deleted

--- src/main/java/com/gallery/gallerymicroservice/client/ProductServiceClient.java
+++ src/main/java/com/gallery/gallerymicroservice/client/ProductServiceClient.java
@@ -4,12 +4,6 @@ import com.gallery.gallerymicroservice.DTO.ProductDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
 import java.util.List;
 
 @FeignClient(name = "product-service",path = "/v1/product/")
