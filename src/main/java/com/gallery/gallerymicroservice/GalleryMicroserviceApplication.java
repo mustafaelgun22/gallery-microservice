4e670c1ec81c77593e06f221e4a046d8e5a48e2a
mustafa <elgunmustafa2@gmail.com>
2023-02-26 17:50:38 +0300

    first commit

---
+++ src/main/java/com/gallery/gallerymicroservice/GalleryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gallery.gallerymicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gallery/gallerymicroservice/GalleryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallery.gallerymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
