6c674efb023717d3d943fca7c6e170df2e3e7e0d
mustafa <elgunmustafa2@gmail.com>
2023-02-28 17:52:23 +0300

    responsestatus added to exceptions

--- src/main/java/com/gallery/gallerymicroservice/Exception/GalleryNotFoundException.java
+++ src/main/java/com/gallery/gallerymicroservice/Exception/GalleryNotFoundException.java
@@ -1,7 +1,8 @@
 package com.gallery.gallerymicroservice.Exception;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class GalleryNotFoundException extends RuntimeException{
 
     public GalleryNotFoundException(String m) {
